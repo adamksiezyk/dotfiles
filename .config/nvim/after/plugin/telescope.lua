6bb678d6de5ad0e68f9d5957523c72200d1f82a4
adamksiezyk <31096588+adamksiezyk@users.noreply.github.com>
2024-11-15 11:18:13 +0100

    Update telescope.lua

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,8 +1,16 @@
+local telescope = require('telescope')
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', function()
-	builtin.find_files({ hidden = true });
-end)
+local themes = require('telescope.themes')
+
+local theme_cursor = themes.get_cursor()
+theme_cursor['jump_type'] = 'never'
+
+vim.keymap.set('n', '<leader>pf', function() builtin.find_files({ hidden = true }); end)
 vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
+vim.keymap.set('n', '<leader>ps', function() builtin.grep_string({ search = vim.fn.input("Grep > ") }); end)
+vim.keymap.set('n', 'gd', function() builtin.lsp_definitions(theme_cursor) end)
+vim.keymap.set('n', 'gr', function() builtin.lsp_references(theme_cursor) end)
+vim.keymap.set('n', '<leader>la', function() builtin.lsp_document_symbols() end)
+vim.keymap.set('n', '<leader>lc', function() builtin.lsp_document_symbols({ symbols = { 'class' } }) end)
+vim.keymap.set('n', '<leader>lf', function() builtin.lsp_document_symbols({ symbols = { 'method', 'function' } }) end)
+vim.keymap.set('n', '<leader>pa', function() builtin.lsp_dynamic_workspace_symbols() end)
