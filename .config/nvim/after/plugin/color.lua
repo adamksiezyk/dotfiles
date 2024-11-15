807c721537cd37c55202f277f9f183dfb90cf355
adamksiezyk <31096588+adamksiezyk@users.noreply.github.com>
2024-11-15 12:25:47 +0100

    Update color.lua

--- .config/nvim/after/plugin/color.lua
+++ .config/nvim/after/plugin/color.lua
@@ -2,7 +2,10 @@ function Color(color)
 	color = color or "rose-pine"
 	require('rose-pine').setup({ disable_italics = true })
 	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 end
 
 Color()
-
